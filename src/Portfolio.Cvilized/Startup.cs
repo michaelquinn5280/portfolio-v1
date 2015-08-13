b63604048ac7bf99322ce93d0ae248f5dfbdf81b
Michael Quinn <michaelquinn5280@live.com>
2015-08-13 15:51:39 -0700

    Azure publish changes

--- src/Portfolio.Cvilized/Startup.cs
+++ src/Portfolio.Cvilized/Startup.cs
@@ -17,10 +17,10 @@ namespace Portfolio.Cvilized
 
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
         }
     }
 }
