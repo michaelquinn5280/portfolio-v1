87a4889f47dd8b9f0dbdb5f7130b82f748a1aa42
Michael Quinn <michaelquinn5280@live.com>
2015-09-02 21:03:18 -0700

    js bug fix in modal controller

--- src/Portfolio.Cvilized/Scripts/Modals/aboutSiteModalController.js
+++ src/Portfolio.Cvilized/Scripts/Modals/aboutSiteModalController.js
@@ -11,11 +11,11 @@
         $scope.items = ['Bootstrap with CVilized wrapper',
                         'AngularJs',
                         'ASP.NET 5 Preview Templates',
-                        '<a class="modal-link" href="http://cvservices.azurewebsites.net/help" target="_blank">Web API</a> with .NET 4.6',
+                        $sce.trustAsHtml('<a class="modal-link" href="http://cvservices.azurewebsites.net/help" target="_blank">Web API</a> with .NET 4.6'),
                         'Application Insights Telemetry',
                         'MongoLab Data',
                         'Hosted in Azure',
-                        'Source available on <a class="modal-link" href="https://github.com/michaelquinn5280/Portfolio" target="_blank">GitHub</a>'];
+                        $sce.trustAsHtml('Source available on <a class="modal-link" href="https://github.com/michaelquinn5280/Portfolio" target="_blank">GitHub</a>')];
         $scope.title = 'SITE SUMMARY';
         $scope.link = '';
         $scope.animationsEnabled = true;
@@ -28,7 +28,7 @@
                 size: size,
                 resolve: {
                     items: function () {
-                        return $sce.trustAsHtml($scope.items);
+                        return $scope.items;
                     },
                     title: function () {
                         return $scope.title;
